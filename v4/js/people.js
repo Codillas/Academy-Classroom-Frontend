c671bbad7813524981393cb10180780c5000d931
Mykola Digtiar <mykola.digtiar@gmail.com>
2025-03-23 22:21:39 +0100

    Add v4

---
+++ v4/js/people.js
@@ -0,0 +1,55 @@
+const token = localStorage.getItem("JWT");
+
+if (token == null) {
+    logout();
+}
+
+const logoutButton = document.querySelector("#logoutButton");
+
+logoutButton.addEventListener("click", function() {
+    logout();
+});
+
+function logout() {
+    localStorage.removeItem("JWT");
+    location.href = "/login.html";
+}
+
+const peopleTableBody = document.querySelector("#peopleTableBody");
+
+async function getPeople() {
+    const response = await fetch("https://academy-classroom-backend.onrender.com/persons", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        }
+    });
+
+    if (response.ok) {
+
+        const persons = await response.json();
+
+        let tableBodyHtml = "";
+        for (let i = 0; i < persons.length; i++) {
+            const person = persons[i];
+            tableBodyHtml += `
+            <tr>
+                <td>${person.id}</td>
+                <td>${person.first_name}</td>
+                <td>${person.last_name}</td>
+                <td>${person.email}</td>
+            </tr>
+        `;
+        }
+
+        peopleTableBody.innerHTML = tableBodyHtml;
+
+    } else {
+        alert("Can not get people!");
+
+        logout();
+    }
+}
+
+getPeople();
---
+++ v4/js/sign-up.js
@@ -0,0 +1,48 @@
+const token = localStorage.getItem("JWT");
+
+if (token != null) {
+    location.href = "/people.html";
+}
+
+const signUpForm = document.querySelector("#signUpForm");
+
+const firstNameInput = document.querySelector("#firstName");
+const lastNameInput = document.querySelector("#lastName");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+
+signUpForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    await signUp();
+});
+
+async function signUp() {
+
+    const firstName = firstNameInput.value;
+    const lastName = lastNameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    const signUpRequestBody = {
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        password: password,
+    };
+
+    const response = await fetch("https://academy-classroom-backend.onrender.com/sign-up", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(signUpRequestBody),
+    });
+
+    if (response.ok) {
+        alert("Sign Up Successfully!");
+        location.href = "/login.html";
+    } else {
+        alert("Sign Up Failed!");
+    }
+}
