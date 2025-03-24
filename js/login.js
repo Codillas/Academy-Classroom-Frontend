73e02063fb992fe934316d42fb6ba1a5145f993c
Mykola Digtiar <mykola.digtiar@gmail.com>
2025-03-24 09:35:57 +0100

    Add backend URL

--- js/login.js
+++ js/login.js
@@ -25,7 +25,7 @@ async function login() {
         password: password,
     };
 
-    const response = await fetch("http://localhost:8080/login", {
+    const response = await fetch("https://academy-classroom-backend-0xay.onrender.com/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
